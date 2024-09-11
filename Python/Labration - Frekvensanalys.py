7694a7f0c741a8e9df29127d6d228d30532314f8
Philip Oberg <philipoberg1994@gmail.com>
2024-09-11 13:24:27 +0200

    Changes to low_pass

--- Python/Labration - Frekvensanalys.py
+++ Python/Labration - Frekvensanalys.py
@@ -7,7 +7,7 @@ def fourier_transform_triangle_wave(numbers_array):
         if n % 2 == 0:
             Bn = 0
         else:
-            Bn = ((8 * 2.5) / (np.pi ** 2) * n) ** ((n - 1) / 2)
+            Bn = ((8 * 2.5) / ((np.pi ** 2) * (n ** 2))) * (-1 ** ((n - 1) / 2))
         print(f"Triangle wave - Bn of n: {n} {Bn:.3f}")
 
 # Function to calculate Fourier transform for sawtooth wave
@@ -18,14 +18,15 @@ def fourier_transform_sawtooth_wave(numbers_array):
         print(f"Sawtooth wave - Bn of n: {n} {Bn:.3f}")
 
     
-# Function to calculate the transfer function of a low-pass filter
 def transfer_function_low_pass_filter(frequency, i, R, C):
     numbers_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    omega = 2 * np.pi * frequency * numbers_array[i]
+    omega = (2 * np.pi) * (frequency * numbers_array[i])
+    j_omega_RC = 1j * omega * R * C
+    H_j_omega = (1 / np.sqrt(j_omega_RC + 1) ** 2)
+    magnitude = abs(H_j_omega)
+    decibel = 20 * np.log10(magnitude)
     print(f"Omega: {omega:.3f}")
-    expression = 1 / np.sqrt(1 + (omega * R * C) ** 2)
-    decibel = 20 * np.log10(expression)
-    print(f"H(jω) = {expression:.3f}")
+    print(f"H(jω) = {magnitude:.3f}")
     print(f"Decibel: {decibel:.3f}")
 
 # Example usage
@@ -33,10 +34,12 @@ numbers_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 frequency = 1000  # 1 kHz
 R = 8200  # 1 kOhm
 C = 10 * 10 ** -9  # 10 nF
-i = 4
+i = 0
 
 fourier_transform_triangle_wave(numbers_array)
 print("")
 fourier_transform_sawtooth_wave(numbers_array)
 print("")
-transfer_function_low_pass_filter(frequency, i, R, C)
+for i in range(len(numbers_array)):
+    print(f"\nn: {numbers_array[i]}")
+    transfer_function_low_pass_filter(frequency, i, R, C)
