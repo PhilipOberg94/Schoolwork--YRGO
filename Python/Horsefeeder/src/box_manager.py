f56daa8d6475694fc1643bd6c32ac738ccb630d7
PhilipOberg94 <153315031+PhilipOberg94@users.noreply.github.com>
2025-03-18 20:29:40 +0100

    Update box_manager.py

--- Python/Horsefeeder/src/box_manager.py
+++ Python/Horsefeeder/src/box_manager.py
@@ -4,12 +4,16 @@ class BoxManager:
     def __init__(self, box_name, initial_time=datetime.strptime("12:00:00", '%H:%M:%S')):
         self.box_name = box_name
         self.boxtime = initial_time
+        self.active = True
 
     def increment_boxtime(self):
         self.boxtime += timedelta(minutes=15)
 
     def decrement_boxtime(self):
         self.boxtime -= timedelta(minutes=15)
+
+    def toggle_active(self):
+        self.active = not self.active
 
     def get_boxtime(self):
         return self.boxtime.strftime('%H:%M')
